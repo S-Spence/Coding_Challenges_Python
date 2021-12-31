1910d542a1880c2376ae3a692fd40b4bcde17f1d
Sarah <sarah.spence1991@gmail.com>
2021-12-31 15:21:41 -0500

    Updating reverse sum

--- Linked_Lists/reversed_sum.py
+++ Linked_Lists/reversed_sum.py
@@ -1,58 +1,103 @@
+import unittest
+"""
+Problem: Given two linked lists storing digits in reverser oder, retrun the sum of the linked lists also in reverse order. 
 
-# Definition for singly-linked list.
+        Ex. 2->4->3
+            5->6->4
+            -------
+            7->0->8
+        Leetcode: https://leetcode.com/problems/add-two-numbers/ 
+Constraints:
+        What to return if the list is empty? -> Assume the list will not be empty
+
+Tests: 
+    l1 = 2->4->3, l2 = 5->6->4 ->  342 + 465 = 807 = result = 7->0->8
+    l1 = 3->0->9, l2 = 0 -> 0-> 2 -> = 1103 + result = 3->0->1->1
+    l1 = 0, l2 = 0 -> reult = 0
+
+Solution: Keep track of the variable to carry on every iteration. Leave the head element as None since the output is in reverse order
+          Return result.next.
+          Runtime O(max(m,n)) Space: O(max(m,n))
+"""
+
 class ListNode:
     def __init__(self, val=None):
          self.val = val
          self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
 
-def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+def add_numbers(l1: ListNode, l2: ListNode) -> ListNode:
     """
-    Take two linked lists in reverse order and add the numbers together. Return 
-    a linked list also in reverse order.
+    Take two linked lists and add the numbers together. Return 
+    a linked list containging the sum in reverse order. 
+    Runtime O(max(m, n)), Space: O(max(m,n)). 
+    Time and space are the longest list in the worst case. 
     """
-    result = ListNode() # create return linked list
-    copy = result       # Create a copy of the linked list to work with
-    carry = 0           # Carry value for sum >= 10
-    
+    result = ListNode()    # Create a linked list to return
+    current = result       # Set the current node value to the results linked list
+    carry = 0              # Carry value for sum >= 10
+
     # While there are numbers to add
     while l1 or l2 or carry:
+        # Iniialize val 1 and val 2 to zero so nothing gets added if one list is shorter
+        val_1 = 0
+        val_2 = 0
         
         # If linked list one has a value, move pointers
         if l1:
-            v1, l1 = l1.val, l1.next
-        else:
-            v1, l1 = 0, None
+            val_1 = l1.val
+            l1 = l1.next
+               
         # If linked list two has a value, move pointers
         if l2:
-            v2, l2 = l2.val, l2.next
-        else:
-            v2, l2 = 0, None
-
+            val_2 = l2.val
+            l2 = l2.next
+       
         # Add total
-        total = carry + v1 + v2
-        # Add value less than ten in copy.next
-        copy.next = ListNode(total % 10)
+        total = carry + val_1 + val_2
+        # Add value less than ten in current.next
+        current.next = ListNode(total % 10)
         # Carry the rest over for the next iteration
-        carry = total // 10
+        carry = total//10
         # Move pointer
-        copy = copy.next
-    
+        current = current.next
+          
+    # print result.next because the sum is in reverse order, so the first node is None. Reverse R/S: O(max(m,n))
     return result.next
- 
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
 
-l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
+# Create L1 elements
+l1_1 = ListNode(2)
+l1_1.next = ListNode(4)
+l1_1.next.next = ListNode(3)
 
-l3 = addTwoNumbers(l1, l2)
+l1_2 = ListNode(3)
+l1_2.next = ListNode(0)
+l1_2.next.next = ListNode(9)
 
-while l3:
-    print(l3.val, end="")
-    l3 = l3.next
+l1_3 = ListNode(0)
+
+# Create L2 elements
+l2_1 = ListNode(5)
+l2_1.next = ListNode(6)
+l2_1.next.next = ListNode(4)
+
+l2_2 = ListNode(0)
+l2_2.next = ListNode(0)
+l2_2.next.next = ListNode(2)
+
+l2_3 = ListNode(0)
+
+# Get results
+l3_1 = add_numbers(l1_1, l2_1)
+l3_2 = add_numbers(l1_2, l2_2)
+l3_3 = add_numbers(l1_3, l2_3)
+
+results = [l3_1, l3_2, l3_3]
+expected = ["708", "3011", "0"]
+
+for i in range(len(results)):
+    while results[i]:
+        print(results[i].val, end="")
+        results[i] = results[i].next
+    print("")
+    print(f"expected: {expected[i]}")
